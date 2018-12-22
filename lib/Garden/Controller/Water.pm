62a3bf830abbda06164d970301585df82a0dabbd
Kyle Conley <kyle.conley25@gmail.com>
2018-12-22 00:27:50 -0600

    status-menu27

--- lib/Garden/Controller/Water.pm
+++ lib/Garden/Controller/Water.pm
@@ -23,6 +23,10 @@ sub status{
     
     my $jsonstuff = $self ->pg->db->query('select data from tp')->hash;
     
+    $jsonstash = $wheeps->stash($jsonstufs);
+    
+    return $jsonstash->render(template => '/water/status', json = $jsonstash);
+    
     return $self->render(template => '/water/status', json => $jsonstuff);
     
     
