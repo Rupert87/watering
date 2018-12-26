871aeb8b40f735785097b6f1a309f41a61968057
Kyle Conley <kyle.conley25@gmail.com>
2018-12-25 22:47:49 -0600

    status-menu42

--- lib/Garden/Controller/Water.pm
+++ lib/Garden/Controller/Water.pm
@@ -24,7 +24,7 @@ sub status{
     my $jsonstuff = $self ->pg->db->query('select info from ord order BY id desc limit 1;')->hash;
     
     
-    return $self->render(template => '/water/status', {json => {$jsonstuff => [2, 'temperature',3]});
+    return $self->render(template => '/water/status', {json => {jsonstuff => [2, 'temperature',3]});
     
     
     
