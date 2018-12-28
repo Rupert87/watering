8f5e53b01e057f86adaced2299712ed1c3824802
Kyle Conley <kyle.conley25@gmail.com>
2018-12-28 00:26:14 -0600

    status-menu75

--- lib/Garden/Controller/Water.pm
+++ lib/Garden/Controller/Water.pm
@@ -31,7 +31,7 @@ sub status{
     #$jsonstuff->stash(json => {info => 'temerature'});
     
     #my $json = $jsonstuff->stash;
-   return $self->render(template => '/water/status', json => $jsonstuff->('temperature'));
+   return $self->render(template => '/water/status', json->('temperature'));
     
     
     
