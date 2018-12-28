26df6879fe4fd2862b107986b1924e69cf5c6a41
Kyle Conley <kyle.conley25@gmail.com>
2018-12-28 00:00:38 -0600

    status-menu55

--- lib/Garden/Controller/Water.pm
+++ lib/Garden/Controller/Water.pm
@@ -23,7 +23,7 @@ sub status{
     
     my $jsonstuff = $self ->pg->db->query('select info from ord order BY id desc limit 1;')->hash;
     
-    my $c->stash(json => $jsonstuff);
+    my $self->stash(json => $jsonstuff);
     
     #$jsonstuff->stash(json => {info => 'temerature'});
     
