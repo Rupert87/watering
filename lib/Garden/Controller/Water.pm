daf67e064ea017ecc83902a5a4e112fa9242f94f
Kyle Conley <kyle.conley25@gmail.com>
2018-12-28 22:46:16 -0600

    menu11

--- lib/Garden/Controller/Water.pm
+++ lib/Garden/Controller/Water.pm
@@ -21,8 +21,8 @@ sub status{
     
     my $self = shift;
     
-    #my $jsonstuff = $self ->pg->db->query('select info from ord order BY id desc limit 1;')->hash;
-    my $jsonstuff = $self ->pg->db->query('select ?::json as fields', {json => {temperature => 'temp'}});
+    my $jsonstuff = $self ->pg->db->query('select info from ord order BY id desc limit 1;');
+    #my $jsonstuff = $self ->pg->db->query('select ?::json as info', {json => {temperature => 'temp'}});
     $self->stash( json => $jsonstuff );
     
     #$self->stash(json => $jsonstuff);
