03757b8c6e8ca9f85c358c3028c7d0544be0d852
Kyle Conley <kyle.conley25@gmail.com>
2018-12-28 23:51:30 -0600

    menu11

--- lib/Garden/Controller/Core.pm
+++ lib/Garden/Controller/Core.pm
@@ -37,33 +37,6 @@ sub menu{
  
  }
 
-sub dispatcher{
-	my $what_to_do = shift;
-	
-	given(lc($what_to_do)){
-		when ('o') {
-			one_time();
-		}
-		when ('r'){
-			list_time();
-		}
-		when ('p'){
-			push_follow_up_boss();
-		}
-		when ('q'){
-			exit;
-		}
-		when ('s'){
-            one_time();
-        }
-        
-        default {
-			say "You didn't pick something I understand, try again. (Press enter to continue)";
-			<STDIN>; #pausing for the user to press enter
-			menu();
-		}
-	}
-	
-}
+
 
 1;
--- watering.pl
+++ watering.pl
@@ -109,3 +109,31 @@ menu();
 
 1;
 
+sub dispatcher{
+	my $what_to_do = shift;
+	
+	given(lc($what_to_do)){
+		when ('o') {
+			one_time();
+		}
+		when ('r'){
+			list_time();
+		}
+		when ('p'){
+			push_follow_up_boss();
+		}
+		when ('q'){
+			exit;
+		}
+		when ('s'){
+            one_time();
+        }
+        
+        default {
+			say "You didn't pick something I understand, try again. (Press enter to continue)";
+			<STDIN>; #pausing for the user to press enter
+			menu();
+		}
+	}
+	
+}
