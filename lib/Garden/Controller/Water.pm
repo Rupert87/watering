d3bd20536f7f343d28f85b0da2c00c69bff85e35
Kyle Conley <kyle.conley25@gmail.com>
2019-01-08 22:59:01 -0600

    water

--- lib/Garden/Controller/Water.pm
+++ lib/Garden/Controller/Water.pm
@@ -1,6 +1,8 @@
 package Garden::Controller::Water;
 use Mojo::Base 'Mojolicious::Controller';
 use Mojo::JSON qw(to_json);
+use Weather::NOAA::GFS;
+use Weather::Underground;
 
 sub connected{
 
@@ -39,5 +41,108 @@ sub status{
     
     
   
+    }
+    
+sub weather_data{
+    
+    my $self = shift; 
+    
+    # define parameters 
+    my %params = (
+                'minlon'   => -5,# mandatory
+                'maxlon'   => 45,# mandatory
+                'minlat'   => 30,# mandatory
+                'maxlat'   => 50,# mandatory
+                'mail_anonymous'    => 'my@mail.org',# mandatory to log NOAA ftp server
+                'gradsc_path' => 'gradsc',# mandatory, needed to create maps
+                'wgrib_path' => 'wgrib',# mandatory, needed to process NOAA GRIB files
+                'timeout'=> '30',#mandatory timeout in minute
+                'debug'    => 1, # 0 no output - 1 output
+                'logfile'    => 'weather-noaa-gfs.log',# optional
+                'cbarn_path' => 'cbarn.gs', #optional, needed to print image legend
+                'r_path' => 'R',# optional, needed to downscale
+                'server_list' => 'nomad3.ncep.noaa.gov,nomad5.ncep.noaa.gov',#optional, server list to choose from   
+                );
+     my $weather_gfs = Weather::NOAA::GFS->new(%params);
+     
+     #download Grib files for your area
+ 
+  if($weather_gfs->downloadGribFiles()){
+        print "downloadGribFiles done!!!";
+  } else {
+        print "Error: downloadGribFiles had problems!!!";
+        die;
+  }
+   
+  #transform Grib files to Ascii files (needs GrADS's wgrib)
+   
+  if($weather_gfs->grib2ascii()){
+        print "grib2ascii succeded!!!";
+  } else {
+        print "Error: grib2ascii had problems!!!";
+        die;
+  }
+ 
+  #transform Ascii files to IDRISI files
+   
+  if($weather_gfs->ascii2idrisi()){
+        print "ascii2idrisi succeded!!!";
+  } else {
+        print "Error: ascii2idrisi had problems!!!";
+        die;
+  }
+ 
+  #Downscale to 0.1 degrees the IDRISI files (needs R)
+  #Execution time has a sensible increase (x3 ca.)
+  if($weather_gfs->idrisiDownscale()){
+        print "idrisiDownscale succeded!!!";
+  } else {
+        print "idrisiDownscale had problems!!!";
+        die;
+  }
+   
+  #itransform Idrisi files to Png images (needs GrADS's gradsc)
+  if($weather_gfs->idrisi2png()){
+        print "idrisi2png succeded!!!";
+  } else {
+        print "Error: idrisi2png had problems!!!";
+        die;
+  }
+ 
+  #Delete files you don't need
+  my @typesToDelete = (
+        "grib",
+        "temp",
+        #"png",
+        "idrisi",
+        );
+$weather_gfs->cleanUp(@typesToDelete);
+
+return $self;
+     
+     
+     
+    }
+    
+sub weather_test{
+    
+    
+   my $weather = Weather::Underground->new(
+        place => "Tonganoxie, Kansas",
+        debug => 1,
+        )
+        || die "Error, could not create new weather object: $@\n";
+ 
+my $arrayref = $weather->get_weather()
+        || die "Error, calling get_weather() failed: $@\n";
+ 
+foreach (@$arrayref) {
+        print "MATCH:\n";
+        while (($key, $value) = each %{$_}) {
+                print "weather";
+        }
+}
+    
+    
     }
 1;
